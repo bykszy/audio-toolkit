994394818780de5f284cfdb608c973d0a4343f83
bykszy <byk@student.agh.edu.pl>
2021-10-16 19:02:22 +0200

    audio plot

--- audio-toolkit/main.py
+++ audio-toolkit/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
 from scipy.io.wavfile import read, write
-import IPython.display as ipd
+from playsound import playsound
 
 fs, x = read('wav-example.wav')
 t = np.arange(0, len(x))/fs
@@ -10,3 +10,5 @@ print(len(t))
 plt.figure(figsize=(20, 7))
 plt.plot(t, x, label='Original signal')
 plt.show()
+
+playsound('audio.mp3')
