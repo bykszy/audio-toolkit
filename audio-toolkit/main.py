c8b52ef4c13bab946b2a76ca7c8779184c4a4642
bykszy <byk@student.agh.edu.pl>
2021-10-24 12:22:33 +0200

    saving audio

--- audio-toolkit/main.py
+++ audio-toolkit/main.py
@@ -2,13 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
 from scipy.io.wavfile import read, write
-from playsound import playsound
+
 
 fs, x = read('wav-example.wav')
+print(fs)
+
 t = np.arange(0, len(x))/fs
-print(len(t))
+
+
 plt.figure(figsize=(20, 7))
 plt.plot(t, x, label='Original signal')
 plt.show()
 
-playsound('audio.mp3')
+write("savetestexample1.wav", fs, x.astype(np.int16))
+
