76c5f8402ab4edf0defce315ec3bca9382a8f6c8
bykszy <byk@student.agh.edu.pl>
2021-10-24 13:55:18 +0200

    adding echo

--- audio-toolkit/main.py
+++ audio-toolkit/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
+from echo import add_echo
 from scipy.io.wavfile import read, write
 
 
@@ -14,5 +15,7 @@ plt.figure(figsize=(20, 7))
 plt.plot(t, x, label='Original signal')
 plt.show()
 
-write("savetestexample1.wav", fs, x.astype(np.int16))
+fs1, x1 = add_echo(fs, x, 100)
+
+write("echotest2.wav", fs1, x1.astype(np.int16))
 
