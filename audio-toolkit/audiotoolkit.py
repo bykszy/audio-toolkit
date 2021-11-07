dad6c0ca4d84a44611665ebd2f6b7157f52cb856
bykszy <byk@student.agh.edu.pl>
2021-11-07 17:22:04 +0100

    librosa v3

--- audio-toolkit/audiotoolkit.py
+++ audio-toolkit/audiotoolkit.py
@@ -6,10 +6,15 @@ def add_echo(fs, x, offset_in_ms):
     offset = int(fs * offset_in_ms / 1000)  # ile próbek pominąć przed dodawaniem echa
     x1 = np.copy(x)
     print(x1)
-    for i in range(len(x) - offset):
-        for j in range(x.shape[1]):
-            x1[i + offset, j] += int(x1[i, j] * 0.4)
+    if x.shape[0] == 2:
+        for i in range(len(x) - offset):
+            for j in range(x.shape[1]):
+                x1[i + offset, j] += int(x1[i, j] * 0.4)
+    else:
+        for i in range(len(x) - offset):
+            x1[i + offset] += int(x1[i] * 0.4)
     return fs, x1
+
 
 
 def add_noise(fs, x, target_noise_db=20, mean_noise=0, seed=-1):
@@ -20,9 +25,10 @@ def add_noise(fs, x, target_noise_db=20, mean_noise=0, seed=-1):
     # noise = np.random.normal(0, 1, (len(x),x.ndim))
 
     target_noise_watts = 10 ** (target_noise_db / 10)
-
-    noise_volts = np.random.normal(mean_noise, np.sqrt(target_noise_watts), (len(x), x.shape[1]))
-
+    if x.shape[0] == 2:
+        noise_volts = np.random.normal(mean_noise, np.sqrt(target_noise_watts), (2, len(x)))
+    else:
+        noise_volts = np.random.normal(mean_noise, np.sqrt(target_noise_watts), len(x))
     print(noise_volts)
     # Noise up the original signal (again) and plot
     y = x + noise_volts
@@ -34,15 +40,23 @@ def add_noise(fs, x, target_noise_db=20, mean_noise=0, seed=-1):
 
 
 def spec_aug(fs, x):
-    xx = np.hsplit(x, 2)
     t = np.arange(0, 1, 1 / 80)
-    x_fft_1 = np.fft.fft(xx[0])
-    x_fft_2 = np.fft.fft(xx[1])
-    fig, axs = plt.subplots(2)
-    fig.suptitle('Spec aug widmo')
-    axs[0].plot(np.abs(x_fft_1))
-    axs[1].plot(np.abs(x_fft_2))
-    plt.show()
+    if x.shape[0] == 2:
+        x_fft_1 = np.fft.fft(x[0])
+        x_fft_2 = np.fft.fft(x[1])
+        fig, axs = plt.subplots(2)
+        fig.suptitle('Spec aug widmo')
+        axs[0].plot(np.abs(x_fft_1))
+        axs[1].plot(np.abs(x_fft_2))
+        plt.show()
+    else:
+        x_fft_1 = np.fft.fft(x)
+        fig, axs = plt.subplots(2)
+        fig.suptitle('Spec aug widmo')
+        axs[0].plot(np.abs(x_fft_1))
+        plt.show()
+
+
 
     # for i in range(len(x)-offset):
     #  for j in range(x.shape[1]):
@@ -56,19 +70,29 @@ def cut10s(fs, x, seconds=10, time_offset=-1):
         seconds = 10
     time = int(fs * seconds)  # ile próbek pobieramy
     if time_offset == -1 or time_offset > len(x):
-        time_offset = np.random.randint(x.shape[0] - time)
-    x1 = np.zeros((time, x.shape[1]))
-    for i in range(time):
-        x1[i] = x[i + time_offset]
+        time_offset = np.random.randint(len(x) - time)
+    if x.shape[0] == 2:
+        x1 = np.zeros((2, time))
+        for i in range(time):
+            for j in range(2):
+                x1[j, i] = x[j, i + time_offset]
+    else:
+        x1 = np.zeros(time)
+        for i in range(time):
+            x1[i] = x[i + time_offset]
     return fs, x1
 
 
 def reverse(fs, x):
-    x1 = np.zeros((len(x), x.shape[1]))
-
-    for i in range(len(x)):
-        for j in range(x.shape[1]):
-            x1[i, j] = x[len(x) - i - 1, j]
+    if x.shape[0] == 2:
+        x1 = np.zeros((2, len(x)))
+        for i in range(len(x)):
+            for j in range(2):
+                x1[j, i] = x[j, len(x) - i - 1]
+    else:
+        x1 = np.zeros(len(x))
+        for i in range(len(x)):
+            x1[i] = x[len(x) - i - 1]
 
     return fs, x1
 
--- audio-toolkit/main.py
+++ audio-toolkit/main.py
@@ -9,22 +9,32 @@ fs = 48000
 
 #fs, x = read('wav-example.wav')
 x, fs_x = librosa.load('wav-example.wav', sr=fs, mono=False)
+
+x_talk, fs_talk = librosa.load('wav-example.wav', sr=fs, mono=False)
+test=np.array(x)
+print(test.shape)
+test.reshape(2, -1)
+print(test.shape)
+x=test.T
+print(x.shape)
 print(x)
 #fs, x_talk = read('record.wav')
-x_toilet,fs_toilet = librosa.load('toilet_flush.wav', sr=fs)
+x_toilet, fs_toilet = librosa.load('toilet_flush.wav', sr=fs)
 
 #t = np.arange(0, len(x_talk)) / fs
 t = np.arange(0, len(x)) / fs_x
 #spec_aug(fs, x_talk)
-spec_aug(fs_x, x)
-spec_aug(fs_toilet, x_toilet)
+#spec_aug(fs_x, x)
+#spec_aug(fs_toilet, x_toilet)
 #spec_aug(fs_resampled, x_resampled)
 plt.figure(figsize=(20, 7))
+
 plt.plot(t, x, label='Original signal')
+plt.show()
 #plt.plot(t, x_talk, label='Original signal')
 write("test1.wav", fs, x.astype(np.int16))
-plt.show()
-"""
+
+
 fs_echo, x_echo = add_echo(fs, x_talk, 100)
 fs_reverse, x_reverse = reverse(fs, x_talk)
 fs_cut10s, x_cut10s = cut10s(fs, x_talk, 10)
@@ -51,4 +61,3 @@ write("record_reverse.wav", fs_reverse, x_reverse.astype(np.int16))
 write("record_cut10s.wav", fs_cut10s, x_cut10s.astype(np.int16))
 write("record_noise.wav", fs_noise, x_noise.astype(np.int16))
 write("record_mixup_speech_toilet.wav", fs_mixup, x_mixup.astype(np.int16))
-"""
