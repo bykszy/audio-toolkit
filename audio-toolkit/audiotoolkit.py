86e11695c686d6113867ac763e30c474de949cbe
bykszy <byk@student.agh.edu.pl>
2021-11-18 22:52:08 +0100

    whole directory loading added

--- audio-toolkit/audiotoolkit.py
+++ audio-toolkit/audiotoolkit.py
@@ -3,182 +3,198 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 import math
+import os
+from scipy.fftpack import fft, ifft
 
 
 class AudioClip:
-    def __init__(self, path, fs=24000):
-        self.path = path
-         #= fs
-        self.x, self.fs = librosa.load(self.path, sr=fs, mono=False)
-        self.x = np.atleast_2d(self.x).T
-        self.x_spec = []
+    def __init__(self, path=None, fs=24000):
+        if path is None:
+            self.path = str(os.path.dirname(os.path.abspath(__file__)))
+            print(self.path)
+        else:
+            self.path = path
 
+        self.audio = []
 
+        if os.path.isdir(self.path):
+            files = librosa.util.find_files(self.path, ext=['wav'])
+            files = np.asarray(files)
+            for y in files:
+                print(str(y))
+                data, sr = librosa.load(y, sr=fs, mono=False)
+                data = np.atleast_2d(data).T
+                self.audio.append([data, sr])
 
+        if os.path.isfile(self.path):
+            data, sr = librosa.load(self.path, sr=fs, mono=False)
+            data = np.atleast_2d(data).T
+            self.audio.append([data, sr])
+            self.x_spec = []
+        self.audio = np.array(self.audio, dtype=object) ####### self.audio format -> [different song (0,1,2,3,...), data(0) or fs(1) ][data samples, channel]
 
     def add_echo(self, offset_in_ms, alfa=0.4):
-        offset = int(self.fs * offset_in_ms / 1000)  # ile próbek pominąć przed dodawaniem echa
-        x1 = np.copy(self.x)
-        for i in range(len(self.x) - offset):
-            for j in range(self.x.shape[1]):
-                self.x[i + offset, j] += self.x[i, j] * alfa
-        return self
-
+        for a in range(len(self.audio)):
+            offset = int(self.audio[a, 1] * offset_in_ms / 1000)  # ile próbek pominąć przed dodawaniem echa
+            x1 = np.copy(self.audio[a, 0])
+            for i in range(len(self.audio[a, 0]) - offset):
+                for j in range(self.audio[a, 0].shape[1]):
+                    self.audio[a, 0][i + offset, j] += self.audio[a, 0][i, j] * alfa
+            return self
 
     def add_noise(self, snr=20, mean_noise=0, seed=-1):
-        if seed != -1:
-            np.random.seed(seed)
+        for a in range(len(self.audio)):
+            if seed != -1:
+                np.random.seed(seed)
 
-        rms_x = math.sqrt(np.mean(self.x ** 2))
-        rms_noise = math.sqrt((rms_x ** 2) / (10 ** (snr / 10)))
-        noise = np.random.normal(mean_noise, rms_noise, self.x.shape)
-        y = np.copy(self.x)
-        self.x = self.x + noise
-        for i in range(len(self.x)):
-            for j in range(self.x.shape[1]):
-                if self.x[i, j] > 0.99:
-                    self.x[i, j] = 0.99
-                if self.x[i, j] < -0.99:
-                    self.x[i, j] = -0.99
+            rms_x = math.sqrt(np.mean(self.audio[a, 0] ** 2))
+            rms_noise = math.sqrt((rms_x ** 2) / (10 ** (snr / 10)))
+            noise = np.random.normal(mean_noise, rms_noise, self.audio[a, 0].shape)
+            y = np.copy(self.audio[a, 0])
+            self.audio[a, 0] = self.audio[a, 0] + noise
+            for i in range(len(self.audio[a, 0])):
+                for j in range(self.audio[a, 0].shape[1]):
+                    if self.audio[a, 0][i, j] > 0.99:
+                        self.audio[a, 0][i, j] = 0.99
+                    if self.audio[a, 0][i, j] < -0.99:
+                        self.audio[a, 0][i, j] = -0.99
         return self
 
+    def erase_freq(self, f_range=100, where_to_begin=50, seed=-1):
+        for a in range(len(self.audio)):
+            if seed != -1:
+                np.random.seed(seed)
 
-    def erase_freq(self, seed=-1, f_range=100, where_to_begin=50):
-        if seed != -1:
-            np.random.seed(seed)
-
-        if self.x.ndim == 2:
-            xx = np.hsplit(self.x, 2)  # podział na kanały
-            t = np.arange(0, 1, 1 / 80)
-            x_fft_1 = np.fft.fft(xx[0])  # kanał lewy
-            x_fft_2 = np.fft.fft(xx[1])  # kanał prawy
-            print("kanał lewy i prawy fft")
-            print(len(x_fft_1))
-            print(len(x_fft_2))
-            fig, axs = plt.subplots(2)
-            fig.suptitle('erase freq widmo')
-            axs[0].plot(np.abs(x_fft_1))
-            axs[1].plot(np.abs(x_fft_2))
-            plt.show()
-            x1 = np.fft.ifft(x_fft_1)
-            x2 = np.fft.ifft(x_fft_2)
-            self.x = np.concatenate((x1.real, x2.real), axis=1)
-        else:
-            x_fft_1 = np.fft.fft(self.x)
-            x_fft_1_after = np.copy(x_fft_1)
-            for i in range(f_range):
-                x_fft_1_after[where_to_begin + i] = 0
-            self.x = np.fft.ifft(x_fft_1_after)
-            fig, axs = plt.subplots(2)
-            fig.suptitle('Spec aug widmo')
-            axs[0].plot(np.abs(x_fft_1))
-            axs[1].plot(np.abs(x_fft_1_after))
-            plt.show()
-            # print(np.sqrt(((x - x_spec) ** 2).mean()))
+            if self.audio[a, 0].ndim == 2:
+                xx = np.hsplit(self.audio[a, 0], 2)  # podział na kanały
+                t = np.arange(0, 1, 1 / 80)
+                a1 = self.audio[a, 0].T[0]
+                a2 = self.audio[a, 0].T[1]# this is a two channel soundtrack, I get the first track
+                #b = [(ele / 2 ** 8.) * 2 - 1 for ele in aa]  # this is 8-bit track, b is now normalized on [-1,1)
+                c1 = fft(a1)
+                c2 = fft(a2) # create a list of complex number
+                d = len(c1) / 2
+                for i in range(f_range):
+                    c1[where_to_begin + i] = 0
+                    c2[where_to_begin + i] = 0
+                fig, axs = plt.subplots(2)
+                fig.suptitle('erase freq widmo')
+                axs[0].plot(abs(c1[:int(d-1)]))
+                axs[1].plot(abs(c1[:int(d-1)]))
+                plt.show()
+                x1 = ifft(c1)
+                x2 = ifft(c2)
+                x1 = np.atleast_2d(x1.real).T
+                x2 = np.atleast_2d(x2.real).T
+                self.audio[a, 0] = np.concatenate((x1.real, x2.real), axis=1)
+            else:
+                x_fft_1 = np.fft.fft(self.audio[a, 0], axis=0)
+                x_fft_1_after = np.copy(x_fft_1)
+                for i in range(f_range):
+                    x_fft_1_after[where_to_begin + i] = 0
+                self.audio[a, 0] = np.fft.ifft(x_fft_1_after)
+                fig, axs = plt.subplots(2)
+                fig.suptitle('Spec aug widmo')
+                axs[0].plot(np.abs(x_fft_1))
+                axs[1].plot(np.abs(x_fft_1_after))
+                plt.show()
+                self.audio[a, 0] = np.fft.ifft(x_fft_1_after)
         return self
-
 
     def specaug(self, spec_or_mel, percent_frequency_erase, percent_time_erase, start_point_frequency=-1,
                 start_point_time=-1, n_fft=2048, hop_length=512, n_mels=128):
-        xx = np.hsplit(self.x, 2)  # podział na kanały
-        if spec_or_mel == 0:
-            # s1 = librosa.amplitude_to_db(np.abs(librosa.stft(xx[0].flatten())), ref=np.max)
-            # s2 = librosa.amplitude_to_db(np.abs(librosa.stft(xx[1].flatten())), ref=np.max)
-            s1 = abs(librosa.stft(xx[0].flatten()))
-            s2 = abs(librosa.stft(xx[1].flatten()))
-        else:
-            s1 = librosa.feature.melspectrogram(xx[0].flatten(), sr=self.fs, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
-            s2 = librosa.feature.melspectrogram(xx[1].flatten(), sr=self.fs, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
-        if start_point_frequency == -1:
-            for i in range(s1.shape[1]):
-                for j in range(int((percent_frequency_erase * s1.shape[0]) / 100)):
-                    s1[int(100 / percent_frequency_erase) * j, i] = 0
-                    s2[int(100 / percent_frequency_erase) * j, i] = 0
-        else:
-            for i in range(s1.shape[1]):
-                for j in range(int((percent_frequency_erase * s1.shape[0]) / 100)):
-                    s1[start_point_frequency + j, i] = 0
-                    s2[start_point_frequency + j, i] = 0
+        for a in range(len(self.audio)):
+            xx = np.hsplit(self.audio[a, 0], 2)  # podział na kanały
+            if spec_or_mel == 0:
+                # s1 = librosa.amplitude_to_db(np.abs(librosa.stft(xx[0].flatten())), ref=np.max)
+                # s2 = librosa.amplitude_to_db(np.abs(librosa.stft(xx[1].flatten())), ref=np.max)
+                s1 = abs(librosa.stft(xx[0].flatten()))
+                s2 = abs(librosa.stft(xx[1].flatten()))
+            else:
+                s1 = librosa.feature.melspectrogram(xx[0].flatten(), sr=self.audio[a, 1], n_fft=n_fft, hop_length=hop_length,
+                                                    n_mels=n_mels)
+                s2 = librosa.feature.melspectrogram(xx[1].flatten(), sr=self.audio[a, 1], n_fft=n_fft, hop_length=hop_length,
+                                                    n_mels=n_mels)
+            if start_point_frequency == -1:
+                for i in range(s1.shape[1]):
+                    for j in range(int((percent_frequency_erase * s1.shape[0]) / 100)):
+                        s1[int(100 / percent_frequency_erase) * j, i] = 0
+                        s2[int(100 / percent_frequency_erase) * j, i] = 0
+            else:
+                for i in range(s1.shape[1]):
+                    for j in range(int((percent_frequency_erase * s1.shape[0]) / 100)):
+                        s1[start_point_frequency + j, i] = 0
+                        s2[start_point_frequency + j, i] = 0
 
-        if start_point_time == -1:
-            for i in range(s1.shape[0]):
-                for j in range(int((percent_time_erase * s1.shape[1]) / 100)):
-                    s1[i, int(100 / percent_time_erase) * j] = 0
-                    s2[i, int(100 / percent_time_erase) * j] = 0
-        else:
-            for i in range(s1.shape[0]):
-                for j in range(int((percent_time_erase * s1.shape[1]) / 100)):
-                    s1[i, start_point_time + j] = 0
-                    s2[i, start_point_time + j] = 0
-        s1_db = librosa.power_to_db(s1, ref=np.max)
-        s2_db = librosa.power_to_db(s2, ref=np.max)
-        # librosa.display.specshow(s1_db, sr=fs, hop_length=hop_length, x_axis='time', y_axis='mel')
-        if spec_or_mel == 0:
-            librosa.display.specshow(s2_db, sr=self.fs, y_axis='linear', x_axis='time')
-            plt.colorbar(format='%+2.0f dB')
-            self.x_spec = np.concatenate((s1_db, s2_db), axis=1)
-        else:
-            librosa.display.specshow(s2_db, sr=self.fs, hop_length=hop_length, x_axis='time', y_axis='mel')
-            plt.colorbar(format='%+2.0f dB')
-            self.x_spec = np.concatenate((s1_db, s2_db), axis=1)
-        plt.show()
+            if start_point_time == -1:
+                for i in range(s1.shape[0]):
+                    for j in range(int((percent_time_erase * s1.shape[1]) / 100)):
+                        s1[i, int(100 / percent_time_erase) * j] = 0
+                        s2[i, int(100 / percent_time_erase) * j] = 0
+            else:
+                for i in range(s1.shape[0]):
+                    for j in range(int((percent_time_erase * s1.shape[1]) / 100)):
+                        s1[i, start_point_time + j] = 0
+                        s2[i, start_point_time + j] = 0
+            s1_db = librosa.power_to_db(s1, ref=np.max)
+            s2_db = librosa.power_to_db(s2, ref=np.max)
+            # librosa.display.specshow(s1_db, sr=fs, hop_length=hop_length, x_axis='time', y_axis='mel')
+            if spec_or_mel == 0:
+                librosa.display.specshow(s2_db, sr=self.audio[a, 1], y_axis='linear', x_axis='time')
+                plt.colorbar(format='%+2.0f dB')
+                self.x_spec[a] = np.concatenate((s1_db, s2_db), axis=1)
+            else:
+                librosa.display.specshow(s2_db, sr=self.audio[a, 1], hop_length=hop_length, x_axis='time', y_axis='mel')
+                plt.colorbar(format='%+2.0f dB')
+                self.x_spec[a] = np.concatenate((s1_db, s2_db), axis=1)
+            plt.show()
         return self
-
 
     def cut(self, seconds=10, padding=0, time_offset=-1):  # padding = 0 uzupelnianie zerami, padding = 1
-        if time_offset == -1 or time_offset > len(self.x):
-            offset = np.random.randint(self.x.shape[0])
-        else:
-            offset = int(self.fs * time_offset)  # ilość próbek offsetu
-        print("fs :" +str(self.fs))
-        time = int(self.fs * seconds)  # ile próbek pobieramy
+        for a in range(len(self.audio)):
+            if time_offset == -1 or time_offset > len(self.audio[a, 0]):
+                offset = np.random.randint(self.audio[a, 0].shape[0])
+            else:
+                offset = int(self.audio[a, 1] * time_offset)  # ilość próbek offsetu
+            time = int(self.audio[a, 1] * seconds)  # ile próbek pobieramy
 
-        if self.x.shape[1] == 2:
-            #print("fs :" + str(self.fs))
-            self.x = self.x[0:time]
-            for i in range(time):
-                for j in range(2):
-                    #x1[i, j] = self.x[i + offset, j]
-                    self.x[i, j] = self.x[i + offset, j]
-                if i + offset == len(self.x) - 1:
-                    break
+            if self.audio[a, 0].shape[1] == 2:
+                self.audio[a, 0] = self.audio[a, 0][0:time]
+                for i in range(time):
+                    for j in range(2):
+                        self.audio[a, 0][i, j] = self.audio[a, 0][i + offset, j]
+                    if i + offset == len(self.audio[a, 0]) - 1:
+                        break
 
-        else:
-            #self.x.resize((time, 1))
-            self.x = self.x[0:time]
-            for i in range(time):
-                #x1[i] = self.x[i + time_offset]
-                self.x[i] = self.x[i + time_offset]
-           # x1 = np.atleast_2d(x1)
-            #x1 = x1.T
+            else:
+                self.audio[a, 0] = self.audio[a, 0][0:time]
+                for i in range(time):
+                    self.audio[a, 0][i] = self.audio[a, 0][i + time_offset]
 
-        if offset + time > self.x.shape[0]:
-            if padding == 1:
-                for i in range(time + offset - self.x.shape[0]):
-                    for j in range(self.x.shape[1]):
-                        #x1[i + self.x.shape[0] - offset - 1, j] = self.x[(i + offset) % self.x.shape[0], j]
-                        self.x[i + self.x.shape[0] - offset - 1, j] = self.x[(i + offset) % self.x.shape[0], j]
-            elif padding == 2:
-                for i in range(time + offset - self.x.shape[0]):
-                    for j in range(x1.shape[1]):
-                        #x1[i + self.x.shape[0] - offset - 1, j] = self.x[self.x.shape[0] - 1 - i % self.x.shape[0], j]
-                        self.x[i + self.x.shape[0] - offset - 1, j] = self.x[self.x.shape[0] - 1 - i % self.x.shape[0], j]
+            if offset + time > self.audio[a, 0].shape[0]:
+                if padding == 1:
+                    for i in range(time + offset - self.audio[a, 0].shape[0]):
+                        for j in range(self.audio[a, 0].shape[1]):
+                            self.audio[a, 0][i + self.audio[a, 0].shape[0] - offset - 1, j] = self.audio[a, 0][
+                                (i + offset) % self.audio[a, 0].shape[0], j]
+                elif padding == 2:
+                    for i in range(time + offset - self.audio[a, 0].shape[0]):
+                        for j in range(self.audio[a, 0].shape[1]):
+                            self.audio[a, 0][i + self.audio[a, 0].shape[0] - offset - 1, j] = self.audio[a, 0][
+                                self.audio[a, 0].shape[0] - 1 - i % self.audio[a, 0].shape[0], j]
         return self
-
 
     def reverse(self):
-        x1 = np.copy(self.x)
-        for i in range(len(self.x)):
-            for j in range(self.x.shape[1]):
-
-                self.x[i, j] = x1[len(self.x) - i - 1, j]
-
+        for a in range(len(self.audio)):
+            x1 = np.copy(self.audio[a, 0])
+            for i in range(len(self.audio[a, 0])):
+                for j in range(self.audio[a, 0].shape[1]):
+                    self.audio[a, 0][i, j] = x1[len(self.audio[a, 0]) - i - 1, j]
         return self
 
-
     def mixup(self, fs1, x1, fs2, x2, alfa):
-      #  fs1, x1 = self.cut(10) #####################################
-      #  fs2, x2 = self.cut(10) #####################################
+        #  fs1, x1 = self.cut(10) #####################################
+        #  fs2, x2 = self.cut(10) #####################################
         if x1.shape[1] == 1 or x2.shape[1] == 1:
             x3 = np.zeros(x1.shape[0])
             for i in range(x1.shape[0]):
@@ -187,25 +203,29 @@ class AudioClip:
             x3 = x1 * alfa + x2 * (1 - alfa)
         return fs1, x1, x2, x3
 
-
     def add_padding(self, desired_len):
-        zeros = np.zeros((desired_len - self.x.shape[0], self.x.shape[1]))
-        self.x = np.concatenate((self.x, zeros), axis=0)
+        for a in range(len(self.audio)):
+            zeros = np.zeros((desired_len - self.audio[a, 0].shape[0], self.audio[a, 0].shape[1]))
+            self.audio[a, 0] = np.concatenate((self.audio[a, 0], zeros), axis=0)
         return self
-
 
     def time_wrap(self, percent):
-        offset = int((percent / 100) * self.x.shape[0])
-        #y = np.copy(self.x)
-        for i in range(self.x.shape[0]):
-            self.x[i] = self.x[(i + offset) % self.x.shape[0]]
+        for a in range(len(self.audio)):
+            offset = int((percent / 100) * self.audio[a, 0].shape[0])
+            # y = np.copy(self.x)
+            for i in range(self.audio[a, 0].shape[0]):
+                self.audio[a, 0][i] = self.audio[a, 0][(i + offset) % self.audio[a, 0].shape[0]]
         return self
 
-    def get_x_fs(self):
-        return self.x, self.fs
+    def get_audio(self):
+        return self.audio
 
-    def get_xspec(self):
+    #def get_x_fs(self):
+     #   for a in range(len(self.audio)):
+      #      return self.x, self.audio[a, 1]
+
+    def get_spectograms(self):
         return self.x_spec
 
-    def get_fs(self):
-        return self.fs
+    #def get_fs(self):
+     #   return self.fs
--- audio-toolkit/main.py
+++ audio-toolkit/main.py
@@ -1,102 +1,19 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.signal
 from audiotoolkit import *
-from scipy.io.wavfile import read, write
-import librosa
+
+xx = AudioClip().cut(10, 0, 0).add_padding(480000).get_audio()
+xx1_and_fs1 = AudioClip('wav-example.wav').cut(10, 0, 0).add_padding(480000).add_echo(100).reverse().add_noise().erase_freq(30000, 500).get_audio()
 
 
-xx, fsx = AudioClip('wav-example.wav').cut(10, 0, 0).get_x_fs()
-xx1, fsx1 = AudioClip('wav-example.wav').cut(10, 0, 0).reverse().get_x_fs()
 
-t = np.arange(0, len(xx)) / fsx
-fig, axs = plt.subplots(2)
+print("xx")
+print(xx)
+print("xxandft")
+print(xx1_and_fs1)
+
+t = np.arange(0, len(xx[0, 0])) / xx[0, 1]
+fig, axs = plt.subplots(len(xx) + 1)
 fig.suptitle('startowe')
-axs[0].plot(t, xx)
-axs[1].plot(t, xx1)
+for i in range(len(xx)):
+    axs[i].plot(t, xx[i, 0])
+axs[len(xx)].plot(t, xx1_and_fs1[0, 0])
 plt.show()
-# fs = 48000
-
-# fs, x = read('wav-example.wav')
-#x, fs_x = librosa.load('wav-example.wav', sr=fs, mono=False)
-
-#x_talk, fs_talk = librosa.load('record.wav', sr=fs, mono=False)
-#x_toilet, fs_toilet = librosa.load('toilet_flush.wav', sr=fs)
-#x = np.atleast_2d(x)
-#x = np.array(x).T
-#x_talk = np.atleast_2d(x_talk)
-#x_talk = np.array(x_talk).T
-#x_toilet = np.atleast_2d(x_toilet)
-#x_toilet = np.array(x_toilet).T
-#fs_mel, x_mel = specaug(fs_x, x)
-#write("record_example_mel.wav", fs_mel, x_mel.astype(np.float32))
-#fs_x_spec, x_spec = specaug(fs_x, x, 0, 10, 20, 2, -1)
-#specaug(fs_x, x, 0, 20, 10, -1, -1)
-"""
-fs_cut1, x_cut1 = cut(fs, x_talk, 30, 0, 0)
-fs_cut2, x_cut2 = cut(fs, x_talk, 30, 1, 0)
-fs_cut3, x_cut3 = cut(fs, x_talk, 30, 2, 0)
-
-fs_reverse, x_reverse = reverse(fs, x_talk)
-t = np.arange(0, len(x_cut1)) / fs_cut1
-fig, axs = plt.subplots(5)
-fig.suptitle('cut 0, cut added, cut reversed')
-axs[0].plot(t, x_cut1)
-axs[1].plot(t, x_cut2)
-axs[2].plot(t, x_cut3)
-axs[3].plot(t, add_padding(x_reverse, x_cut1.shape[0]))
-axs[4].plot(t, add_padding(time_wrap(x_talk, 50), x_cut1.shape[0]))
-plt.show()
-
-print(x_talk.shape)
-print(x.shape)
-print(x_toilet.shape)
-print(x_toilet)
-t1 = np.arange(0, len(x)) / fs_x
-t2 = np.arange(0, len(x_talk)) / fs_talk
-t3 = np.arange(0, len(x_toilet)) / fs_toilet
-# spec_aug(fs, x_talk)
-# fs_x_spec, x_spec = erase_freq(fs_x, x)
-# erase_freq(fs_toilet, x_toilet)
-# erase_freq(fs_resampled, x_resampled)
-fig, axs = plt.subplots(3)
-fig.suptitle('startowe')
-axs[0].plot(t1, x)
-axs[1].plot(t2, x_talk)
-axs[2].plot(t3, x_toilet)
-plt.show()
-# plt.plot(t, x_talk, label='Original signal')
-write("test1.wav", fs, x.astype(np.int16))
-
-
-fs_echo, x_echo = add_echo(fs, x_talk, 100)
-
-fs_reverse, x_reverse = reverse(fs, x_talk)
-
-fs_cut, x_cut = cut(fs, x_talk, 10)
-
-fs_noise, x_noise = add_noise(fs, x_talk, 10)
-
-fs_mixup, x_mixup_1, x_mixup_2, x_mixup = mixup(fs, x_talk, fs_toilet, x_toilet, 0.8)
-fig, axs = plt.subplots(3)
-fig.suptitle('1, 2, Mixup')
-axs[0].plot(np.arange(0, len(x_mixup_1)) / fs_mixup, x_mixup_1)
-axs[1].plot(np.arange(0, len(x_mixup_2)) / fs_mixup, x_mixup_2)
-axs[2].plot(np.arange(0, len(x_mixup)) / fs_mixup, x_mixup)
-
-plt.show()
-
-fig, axs = plt.subplots(5)
-fig.suptitle('1-Normal, 2-echo, 3-reverse, 4-cut, 5-noise')
-axs[0].plot(np.arange(0, len(x_talk)) / fs, x_talk)
-axs[1].plot(np.arange(0, len(x_echo)) / fs_echo, x_echo)
-axs[2].plot(np.arange(0, len(x_reverse)) / fs_reverse, x_reverse)
-axs[3].plot(np.arange(0, len(x_cut)) / fs_cut, x_cut)
-axs[4].plot(np.arange(0, len(x_noise)) / fs_noise, x_noise)
-plt.show()
-write("record_echo.wav", fs_echo, x_echo.astype(np.float32))
-write("record_reverse.wav", fs_reverse, x_reverse.astype(np.float32))
-write("record_cut.wav", fs_cut, x_cut.astype(np.float32))
-write("record_noise.wav", fs_noise, x_noise.astype(np.float32))
-write("record_mixup_speech_toilet.wav", fs_mixup, x_mixup.astype(np.float32))
-"""
